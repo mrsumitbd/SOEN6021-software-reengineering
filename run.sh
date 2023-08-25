2e1b2fa44b35d334ff493c7db2c210352deccbf9
Musfiqur Rahman <musfiqurrahman.lab08@gmail.com>
2023-08-24 22:02:10 -0400

    adding new project

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python src/run_repo_in_ci_env.py lagom master 25 create_new
-python src/run_repo_in_ci_env.py lagom master 25 append
+python src/run_repo_in_ci_env.py entropy master 25 create_new
+python src/run_repo_in_ci_env.py entropy master 25 append
