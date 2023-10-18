fff617ef3391dab0d88c358df84e35f0f90f2832
Musfiqur Rahman <musfiqurrahman.lab08@gmail.com>
2023-10-18 08:09:04 -0400

    adding pyglmnet data

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python src/run_repo_in_ci_env.py pyalcs master 25 create_new
-python src/run_repo_in_ci_env.py pyalcs master 25 append
+python src/run_repo_in_ci_env.py pyglmnet master 25 create_new
+python src/run_repo_in_ci_env.py pyglmnet master 25 append
