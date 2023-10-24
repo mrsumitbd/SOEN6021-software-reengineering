5953fe88a3fd3bd925e4acf601922a5404701b9a
Musfiqur Rahman <musfiqurrahman.lab08@gmail.com>
2023-10-23 23:07:59 -0400

    updating healthcareai data

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python src/run_repo_in_ci_env.py healthcareai-py master 25 create_new
-python src/run_repo_in_ci_env.py healthcareai-py master 25 append
+python src/run_repo_in_ci_env.py pymfe master 25 create_new
+python src/run_repo_in_ci_env.py pymfe master 25 append
 
-# python src/run_repo_in_ci_env.py pymc-learn master 25 create_new
-# python src/run_repo_in_ci_env.py pymc-learn master 25 append
+python src/run_repo_in_ci_env.py regain master 25 create_new
+python src/run_repo_in_ci_env.py regain master 25 append
