bfbe8b206c20614f75a29be98f905044855f86ca
Musfiqur Rahman <musfiqurrahman.lab08@gmail.com>
2023-10-27 19:03:30 -0400

    adding new project data

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python src/run_repo_in_ci_env.py regain master 25 create_new
-python src/run_repo_in_ci_env.py regain master 25 append
+python src/run_repo_in_ci_env.py kernel-gof master 25 create_new
+python src/run_repo_in_ci_env.py kernel-gof master 25 append
 
 
-python src/run_repo_in_ci_env.py pymfe master 25 create_new
-python src/run_repo_in_ci_env.py pymfe master 25 append
+#python src/run_repo_in_ci_env.py pymfe master 25 create_new
+#python src/run_repo_in_ci_env.py pymfe master 25 append
